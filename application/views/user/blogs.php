b0153e406d8c6041a25b333cba76c334d140a4a8
KumarDalvi <kumardalvi2019@gmail.com>
2026-01-28 18:23:12 +0530

    Blogs.php Completed
    Blogs page Completed

---
+++ application/views/user/blogs.php
@@ -0,0 +1,38 @@
+<section class="container-fluid py-5">
+    <div class="row px-lg-5 px-3 align-items-center blog-item">
+
+        <div class="col-md-6 py-4 order-1 order-md-1 text-center">
+            <img src="<?= base_url('assets/image/blog1.jpg') ?>" class="blog_img img-fluid" alt="">
+        </div>
+
+        <div class="col-md-6 py-4 order-2 order-md-2">
+            <h2 class="blog-title">
+                Chairperson & Joint MD, Hardik Kothiya, Shines on the Hurun List
+            </h2>
+            <p class="blog-desc">
+                Rayzon solar firmly and proudly believes in ‘success is brighter when it lights up an entire industry—not just an individual’.
+            </p>
+            <a href="#" class="read_more_btn mt-2">Read More</a>
+        </div>
+
+    </div>
+
+    <!-- 2nd row (Repeat)-->
+    <div class="row px-lg-5 px-3 align-items-center blog-item">
+
+        <div class="col-md-6 py-4 order-1 order-md-1 text-center">
+            <img src="<?= base_url('assets/image/blog2.jpg') ?>" class="blog_img img-fluid" alt="">
+        </div>
+
+        <div class="col-md-6 py-4 order-2 order-md-2">
+            <h2 class="blog-title">
+                Chairperson & Joint MD, Hardik Kothiya, Shines on the Hurun List
+            </h2>
+            <p class="blog-desc">
+                Rayzon solar firmly and proudly believes in ‘success is brighter when it lights up an entire industry—not just an individual’.
+            </p>
+            <a href="#" class="read_more_btn mt-2">Read More</a>
+        </div>
+
+    </div>
+</section>
