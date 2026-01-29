e97977ca577b08d74dad36a05889140e90930217
Yogesh <yogeshkolate1@gamil.com>
2026-01-29 11:27:36 +0530

    done
    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,3 +22,38 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(changeImage, 3000);
 
 });
+
+
+  const faqItems = document.querySelectorAll(".faq-item");
+
+  faqItems.forEach((item) => {
+    item.addEventListener("click", () => {
+
+      // Close all
+      faqItems.forEach((other) => {
+        other.classList.remove("active");
+      });
+
+      // Open clicked one
+      item.classList.add("active");
+    });
+  });
+
+
+
+document.querySelectorAll("#faq-section .faq-col:first-child .faq-q").forEach(q => {
+
+  q.addEventListener("click", () => {
+
+    // close all
+    document.querySelectorAll("#faq-section .faq-col:first-child .faq-q")
+      .forEach(item => item.classList.remove("active"));
+
+    document.querySelectorAll("#faq-section .faq-answer")
+      .forEach(ans => ans.classList.remove("show"));
+
+    // open clicked
+    q.classList.add("active");
+    q.nextElementSibling.classList.add("show");
+  });
+});
